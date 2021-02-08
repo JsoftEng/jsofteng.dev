f23ea6ec8c74eb2cc07d2e805ad4ccd627ae9d50
John Johnson <jsofteng@gmail.com>
2021-02-08 11:35:47 -0500

    Added portfolio to project section, added alts for images

--- src/components/layout/Section/index.jsx
+++ src/components/layout/Section/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import DownArrow from '../../../img/down-arrow.svg'
 import portrait from '../../../img/portrait.png'
+import ProjectPanel from '../../utility/ProjectPanel'
 
 import './section.scss'
 
@@ -14,39 +15,44 @@ function Section (props) {
     case 'Home':
       sectionClass = 'home-section'
       sectionID = 'home'
-      content = <>
-        <div className='avatar'>
-          <h1>J</h1>
-        </div>
-        <a id='down-arrow' href='#about'><DownArrow className='down-arrow'/></a>
-      </>
+      content =
+        <>
+          <div className='avatar'>
+            <h1>J</h1>
+          </div>
+          <a id='down-arrow' href='#about'><DownArrow className='down-arrow'/></a>
+        </>
       break
     case 'About':
       sectionClass = 'about-section'
       sectionID = 'about'
-      content = <>
-        <div className='portrait'>
-          <img src={ portrait }></img>
-        </div>
-        <div className='about-section-text'>
-          <h2>Who Am I?</h2>
-          <hr/>
-          <p>
-            Hi, I&apos;m John, a full-stack software engineer with a passion for front-end development.
-            I&apos;ve been a professional software developer for a little over 4 years now. In that time I&apos;ve had the luxury
-            of being exposed to a number of tech stacks ranging from ASP.Net/Angular to React on Rails. I&apos;m no stranger to agility!
-          </p>
-          <p>
-            When I&apos;m not knee deep in code I&apos;m usually spending time with my dog Kaiser, gaming, watching anime or on some wild
-            adventure with a couple of friends.
-          </p>
-        </div>
-      </>
+      content =
+        <>
+          <div className='portrait'>
+            <img src={ portrait } alt='Portrait'></img>
+          </div>
+          <div className='about-section-text'>
+            <h2>Who Am I?</h2>
+            <hr/>
+            <p>
+              Hi, I&apos;m John, a full-stack software engineer with a passion for front-end development.
+              I&apos;ve been a professional software developer for a little over 4 years now. In that time I&apos;ve had the luxury
+              of being exposed to a number of tech stacks ranging from ASP.Net/Angular to React on Rails. I&apos;m no stranger to agility!
+            </p>
+            <p>
+              When I&apos;m not knee deep in code I&apos;m usually spending time with my dog Kaiser, gaming, watching anime or on some wild
+              adventure with a couple of friends.
+            </p>
+          </div>
+        </>
       break
     case 'Projects':
       sectionClass = 'projects-section'
       sectionID = 'projects'
-      content = <></>
+      content =
+        <>
+          <ProjectPanel id='portfolio-project' name='portfolio-project'/>
+        </>
       break
     case 'Contact':
       sectionClass = 'contact-section'
---
+++ src/components/utility/ProjectPanel/index.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import PortfolioThumbnail from '../../../img/project-thumbnails/portfolio-thumbnail.png'
+
+import './project-panel.scss'
+
+function ProjectPanel (props) {
+  let projectURL = null
+  let projectTitle = null
+  let projectThumbnail = null
+  let projectThumbnailAltText = null
+  let projectDescription = null
+  let projectTech = null
+  let techList = null
+
+  switch (props.name) {
+    case 'portfolio-project':
+      projectTitle = 'Portfolio'
+      projectURL = 'https://jsofteng.dev'
+      projectThumbnail = PortfolioThumbnail
+      projectThumbnailAltText = 'Portfolio Thumbnail'
+      projectDescription = 'My personal portfolio site. I wanted to take a shot at the neumorphic aesthetic, so I decided that my portfolio was as good a project as any!'
+      projectTech = [
+        {
+          id: '1',
+          name: 'HTML',
+          url: 'https://html.spec.whatwg.org/'
+        },
+        {
+          id: '2',
+          name: 'SASS',
+          url: 'https://sass-lang.com/'
+        },
+        {
+          id: '3',
+          name: 'Javascript',
+          url: null
+        },
+        {
+          id: '4',
+          name: 'React',
+          url: 'https://reactjs.org/'
+        },
+        {
+          id: '5',
+          name: 'Babel',
+          url: 'https://babeljs.io/'
+        },
+        {
+          id: '6',
+          name: 'Webpack',
+          url: 'https://webpack.js.org/'
+        }
+      ]
+      break
+  }
+
+  techList = projectTech.map((tech) => {
+    return (
+      <a key={tech.id} href={tech.url} target='_blank' rel='noreferrer' className='project-tech'>
+        <h2>{tech.name}</h2>
+      </a>
+    )
+  })
+
+  const content =
+    <>
+      <div id='project-panel' className='project-panel'>
+        <div id='project-panel-main' className='project-panel-main'>
+          <a href={projectURL}>
+            <div id='project-thumbnail' className='project-thumbnail'>
+              <img src={projectThumbnail} alt={projectThumbnailAltText}/>
+            </div>
+          </a>
+          <h2>{projectTitle}</h2>
+        </div>
+        <div id='project-panel-info' className='project-panel-info'>
+          <p id='project-description' className='project-description'>
+            {projectDescription}
+          </p>
+          <div id='project-tech-container' className='project-tech-container'>{techList}</div>
+        </div>
+      </div>
+    </>
+  return (
+    content
+  )
+}
+
+export default ProjectPanel
