3665ef9b05c43c7ba1969d7d94dbfaf0f52e184d
John Johnson <jsofteng@gmail.com>
2021-02-13 14:30:30 -0500

    Added contact form, modified google font api call

--- src/components/layout/Section/index.jsx
+++ src/components/layout/Section/index.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import DownArrow from '../../../img/down-arrow.svg'
 import portrait from '../../../img/portrait.webp'
 import ProjectPanel from '../../utility/ProjectPanel'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 
 import './section.scss'
 
@@ -10,6 +11,7 @@ function Section (props) {
   let sectionClass = null
   let sectionID = null
   let content = null
+  let validate = null
 
   switch (props.name) {
     case 'Home':
@@ -57,7 +59,66 @@ function Section (props) {
     case 'Contact':
       sectionClass = 'contact-section'
       sectionID = 'contact'
-      content = <></>
+      validate = values => {
+        const errors = {}
+
+        if (!values.name) {
+          errors.name = 'Required'
+        }
+
+        if (!values.email) {
+          errors.email = 'Required'
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+          errors.email = 'Invalid Email Address'
+        }
+
+        if (!values.message) {
+          errors.message = 'Required'
+        }
+
+        return errors
+      }
+      content =
+        <>
+          <Formik
+            initialValues= {{
+              name: '',
+              email: '',
+              message: ''
+            }}
+            validate={validate}
+            onSubmit= {(values, { setSubmitting }) => {
+              setTimeout(() => {
+                setSubmitting(false)
+              }, 400)
+            }}
+          >
+            <Form data-netlify='true' className='contact-form'>
+              <div id='form-name-container' className='form-name-container'>
+                <label htmlFor='name'>Name</label>
+                <Field name='name' type='text'/>
+                <ErrorMessage name='name'>
+                  {msg => <div id='form-name-error' className='form-error'>{msg}</div>}
+                </ErrorMessage>
+              </div>
+              <div id='form-email-container' className='form-email-container'>
+                <label htmlFor='email'>Email</label>
+                <Field name='email' type='text'/>
+                <ErrorMessage name='email'>
+                  {msg => <div id='form-email-error' className='form-error'>{msg}</div>}
+                </ErrorMessage>
+              </div>
+              <div id='form-message-container' className='form-message-container'>
+                <label htmlFor='message'>Message</label>
+                <Field name='message' as='textarea' type='text'/>
+                <ErrorMessage name='message'>
+                  {msg => <div id='form-message-error' className='form-error'>{msg}</div>}
+                </ErrorMessage>
+              </div>
+              <button className='form-submit' type='submit'>Submit</button>
+            </Form>
+          </Formik>
+        </>
       break
     default:
       break
